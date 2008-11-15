8a6761f62ffe6ac1220b186d27adf6ab1cdfc2cc
glandrum <glandrum@eb9a5bb2-52ac-11de-9335-abd5230c83eb>
2008-11-15 16:35:29 +0000

    create branch for mingw work

---
+++ branches/mingwBuild_15Nov2008/Code/GraphMol/RDKitBase.h
@@ -0,0 +1,29 @@
+//
+//  Copyright (C) 2002-2008 Greg Landrum and Rational Discovery LLC
+//
+//   @@ All Rights Reserved  @@
+//
+
+/*! \file RDKitBase.h
+
+  \brief pulls in the core \c RDKit functionality
+
+*/
+
+#ifndef _RDKIT_BASE_H
+#define _RDKIT_BASE_H
+
+#include <RDGeneral/Invariant.h>
+
+#include <GraphMol/Atom.h>
+#include <GraphMol/Bond.h>
+#include <GraphMol/Conformer.h>
+#include <GraphMol/GraphMol.h>
+#include <GraphMol/MolOps.h>
+#include <GraphMol/RingInfo.h>
+#include <GraphMol/AtomIterators.h>
+#include <GraphMol/BondIterators.h>
+#include <GraphMol/PeriodicTable.h>
+#include <GraphMol/SanitException.h>
+
+#endif
