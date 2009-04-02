24d2b6f38757c10d4a1d06d8580b0367e11f0bfc
glandrum <glandrum@eb9a5bb2-52ac-11de-9335-abd5230c83eb>
2009-04-02 03:52:50 +0000

    obsolete

--- branches/CanonicalizerRework30Nov/Python/Chem/Pharm2D/__init__.py
+++
@@ -1,6 +0,0 @@
-#
-# Copyright (C) 2002 greg Landrum and Rational Discovery LLC
-#
-""" module with functionality for 2D pharmacophores
-
-"""
--- branches/CanonicalizerRework30Nov/Python/ML/FeatureSelect/test_list.py
+++
@@ -1,9 +0,0 @@
-
-tests=[
-  ("python","UnitTestCMIM.py",{}),
-  ]
-
-
-
-longTests=[
-  ]
